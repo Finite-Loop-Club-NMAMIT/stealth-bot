9f0330587598b05dd46595203d5d56da67c6ba4e
Rakshith <rakshithx09@gmail.com>
2023-10-11 21:10:40 +0530

    code cleanup

--- public/script.js
+++
@@ -1,7 +0,0 @@
-const switchBtn = document.querySelector('.switch');
-const toggle = document.getElementById('toggle');
-const root = document.documentElement;''
-toggle.addEventListener('click', () => {
-    switchBtn.classList.toggle('darkMode');
-    root.classList.toggle('dark');
-});
